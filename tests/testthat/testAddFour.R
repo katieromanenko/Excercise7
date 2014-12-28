24c4b2150c311b78851e6ccb6103216e3c7a0b4b
Katie Romanenko <katie.romanenko@gmail.com>
2014-12-28 17:28:39 +0100

    improved

--- tests/testthat/testAddFour.R
+++ tests/testthat/testAddFour.R
@@ -2,3 +2,9 @@ library(testthat)
 
 x <- 5 #x can be any numerical number you choose
 expect_that(AddFour(x), equals(x+4))
+
+x <- -3
+expect_that(AddFour(x), equals(x+4))
+
+x <- 5.45
+expect_that(AddFour(x), equals(x+4))
--- tests/testthat/testAddTwo.R
+++ tests/testthat/testAddTwo.R
@@ -2,3 +2,9 @@ library(testthat)
 
 x <- 5 #x can be any numeric value you want to use the function for
 expect_that(AddTwo(x), equals(x+2))
+
+x <- -2
+expect_that(AddTwo(x), equals(x+2))
+
+x <- 6.87
+expect_that(AddTwo(x), equals(x+2))
